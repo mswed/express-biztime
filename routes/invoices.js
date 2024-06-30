2703222b2f3dcb0edb80fc6051f3850ad600e315
Moshe Swed <mswed@beapot.com>
2024-06-30 19:19:26 -0400

    added get invoice by id

--- routes/invoices.js
+++ routes/invoices.js
@@ -15,58 +15,58 @@ router.get('/', async (req, res, next) => {
 
 })
 
-// Get company by id
-router.get('/:code', async (req, res, next) => {
+// Get invoice by id
+router.get('/:id', async (req, res, next) => {
     try {
-        const {code} = req.params;
-        const results = await db.query(`SELECT * FROM invoices WHERE code = $1`, [code]);
+        const {id} = req.params;
+        const results = await db.query(`SELECT * FROM invoices WHERE id = $1`, [id]);
         if (results.rows.length === 0) {
-            return next(new ExpressError(`Can not find a company with code of ${code}`, 404))
+            return next(new ExpressError(`Can not find a invoice with id of ${id}`, 404))
         }
 
-        return res.json({company: results.rows[0]})
+        return res.json({invoice: results.rows[0]})
     } catch (e) {
         next(e)
     }
 })
 
-// Create a new company
+// Create a new invoice
 router.post('/', async (req, res, next) => {
     try {
-        const {code, name, description} = req.body;
-        const results = await db.query(`INSERT INTO invoices (code, name, description) VALUES ($1, $2, $3) RETURNING *`, [code, name, description])
-        return res.status(201).json({company: results.rows[0]})
+        const {id, name, description} = req.body;
+        const results = await db.query(`INSERT INTO invoices (id, name, description) VALUES ($1, $2, $3) RETURNING *`, [id, name, description])
+        return res.status(201).json({invoice: results.rows[0]})
     } catch (e) {
         next(e)
     }
 })
 
-// Update a company by code
-router.put('/:code', async (req, res, next) => {
+// Update a invoice by id
+router.put('/:id', async (req, res, next) => {
     try {
-        const {code} = req.params;
+        const {id} = req.params;
         const {name, description} = req.body;
-        const results = await db.query(`UPDATE invoices SET name = $1, description = $2 WHERE code = $3 RETURNING *`, [name, description, code])
+        const results = await db.query(`UPDATE invoices SET name = $1, description = $2 WHERE id = $3 RETURNING *`, [name, description, id])
         if (results.rows.length === 0) {
-            return next(new ExpressError(`Can not update company with code of ${code}`, 404))
+            return next(new ExpressError(`Can not update invoice with id of ${id}`, 404))
         }
 
-        return res.json({company: results.rows[0]})
+        return res.json({invoice: results.rows[0]})
     } catch (e) {
         next(e)
     }
 })
 
-// Delete a company by id
-router.delete('/:code', async (req, res, next) => {
+// Delete a invoice by id
+router.delete('/:id', async (req, res, next) => {
     try {
-        const {code} = req.params;
-        const valid = await db.query(`SELECT * FROM invoices WHERE code = $1`, [code])
+        const {id} = req.params;
+        const valid = await db.query(`SELECT * FROM invoices WHERE id = $1`, [id])
         if (valid.rows.length > 0) {
-            await db.query(`DELETE FROM invoices WHERE code = $1`, [code])
+            await db.query(`DELETE FROM invoices WHERE id = $1`, [id])
             return res.json({status: 'deleted'})
         } else {
-            throw new ExpressError(`Can not delete company with code of ${code}`, 404)
+            throw new ExpressError(`Can not delete invoice with id of ${id}`, 404)
 
         }
 
