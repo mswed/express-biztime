db4eebd42165f958873d6d2bf46de2b3710ff4dd
Moshe Swed <mswed@beapot.com>
2024-06-30 18:23:38 -0400

    step 1

--- db.js
+++ db.js
@@ -1,2 +1,17 @@
 /** Database setup for BizTime. */
 
+const { Client } = require('pg');
+
+let DB_URI;
+
+if (process.env.NODE_ENV === 'test') {
+    DB_URI = 'postgressql:///biztime_test';
+} else {
+    DB_URI = 'postgressql:///biztime';
+}
+
+let db = new Client({connectionString: DB_URI});
+
+db.connect();
+
+module.exports = db;
